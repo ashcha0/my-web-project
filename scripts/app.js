c557cc1b0361831db184170b811f169e29b3876d
zhanglechao <1473085366@qq.com>
2025-01-19 22:13:49 +0800

    Initial:添加项目基础文件

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+// 简单的交互示例
+document.addEventListener('DOMContentLoaded', function () {
+    const introSection = document.querySelector('.intro');
+
+    introSection.addEventListener('click', function () {
+        alert('你点击了介绍部分！');
+    });
+});
